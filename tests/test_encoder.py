cb54019a207d641e235dfd88e9729cbf5b2845c0
Enrico Lovisotto <enricolovisotto@gmail.com>
2017-12-06 18:16:54 +0100

    Fixed tests to be run inside Travis

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -9,33 +9,12 @@ import LDPC
 from specs import *
 
 
-def test_encoding_np():
-    ''' Test encoding using dense matrices '''
-    for n in get_code_lengths():
-        for rate in get_code_rates():
-            H = get_expanded_H_matrix(n, rate) # (n-k) x n
-
-            n = H.shape[1]
-            k = n - H.shape[0]
-
-            B = H[0:(n-k):1, 0:k:1] # (n-k) x k
-            C = H[0:(n-k):1, k:n:1] # (n-k) x (n-k)
-
-            A = npla.inv(C).dot(B)
-
-            G = np.vstack((np.eye(k), A))
-
-            for trial in range(0, 10):
-                i = random.randrange(0, 2**k)
-                u = np.array([int(a) for a in np.binary_repr(i, width=k)])
-
-                c = G.dot(u)
-                if sum(H.dot(c) % 2) != 0:
-                    raise ValueError("Error for n={}, rate={}, trial={}".format(n, rate, trial))
-
-def test_encoding_lib():
+def test_encoding():
     ''' Test encoding using sparse matrices of LDPC module '''
-    for n in get_code_lengths():
+
+    # test a subset of code lengths, in order to
+    # keep testing fast enough
+    for n in random.sample(get_code_lengths(), 5):
         for rate in get_code_rates():
             H = get_expanded_H_matrix(n, rate) # (n-k) x n
             n = H.shape[1]
@@ -43,10 +22,12 @@ def test_encoding_lib():
 
             encoder = LDPC.encoder(H)
 
-            for trial in range(0, 10):
-                i = random.randrange(0, 2**k)
-                u = np.array([int(a) for a in np.binary_repr(i, width=k)])
+            for trial in range(0, 50):
+                u = np.random.choice(a=[0, 1], size=k, p=[1/2, 1/2])
 
                 c = encoder(u)
                 if sum(H.dot(c) % 2) != 0:
-                    raise ValueError("Error for n={}, rate={}, trial={}".format(n, rate, trial))
+                    raise ValueError(
+                        "Error for n={}, rate={}, trial={}"
+                        .format(n, rate, trial)
+                    )
