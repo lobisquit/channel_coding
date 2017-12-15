b8b32a04d31f7128c9c37ae55c0f8d1367899b1d
Enrico Lovisotto <enricolovisotto@gmail.com>
2017-12-15 11:23:34 +0100

    Added decoder building function
    Note that, same as encoder, this is an higher order function
    that given code and channel parameters ouputs the effective
    message passing decoder

--- LDPC/decoder.pyx
+++ LDPC/decoder.pyx
@@ -1,10 +1,9 @@
+import cython
+
 import numpy as np
-import scipy.sparse as sp
-
-from libc.math cimport sqrt, exp, log, pow
-
-cimport cython
 cimport numpy as np
+
+from libc.math cimport sqrt, exp, log, pow, abs
 
 def is_codeword(H, c):
     '''
@@ -26,9 +25,129 @@ def is_codeword(H, c):
     parity_check_bits = H.dot(c) % 2
     return np.all(parity_check_bits == 0)
 
+from time import time
+
+import numpy as np
+import scipy.sparse as sp
+
+import LDPC
+import specs
+
+H = specs.get_expanded_H_matrix(2304, '1/2')
+H = LDPC.SPMatrix(H)
+
+n = H.shape[1]
+
+def decoder(H, sigma_w, u_distrib=None, max_iterations=10):
+    '''
+    Create function to decode parity check code desctribed by H
+
+    Parameters
+    ----------
+    H : LDPC.SPMatrix
+        Parity check matrix of code
+
+    sigma_w : float
+        Noise variance (true or estimated)
+
+    u_distrib : np.ndarray, optional
+        Array of length k where i-th element contains P[u_i == 0]
+        Default value is uniform distribution for all bits
+
+    max_iterations : int, optional
+        Number of iterations of message passing algorithm
+        Default value is 10
+
+    Returns
+    -------
+    callable
+        Function that receives a real numbered channel measures
+        array and that returns transmitted message estimate
+    '''
+    # code information
+    n = H.shape[1]
+    k = n - H.shape[0]
+
+    # set uniform if input a priori distribution
+    # is not given
+    if u_distrib == None:
+        u_distrib = np.empty((k,))
+        u_distrib.fill(0.5)
+
+    # fill channel LLR vector with a priori information
+    message_a_priori_LLR = np.log(u_distrib) - np.log(1 - u_distrib)
+
+    # a priori LLR is 0 for the parity check bits
+    a_prori_LLR = np.concatenate((message_a_priori_LLR, np.zeros(n-k)))
+
+    # create matrices to store backward and forward messages
+    F = H.copy()
+    B = H.copy()
+
+    ### INIT
+    # set all backward messages as constants (TODO check if 0 is good)
+    B.update_all(lambda x: x*0)
+
+    def decode(r):
+        ### A PRIORI -> initialize a priori channel knowledge
+        ch = -2 * r / sigma_w**2 + a_prori_LLR
+
+        for _ in range(max_iterations):
+            # compute Fij using this precomputed vector b
+            b = B.apply_on_rows(sum)
+
+            ### ESTIMATION -> marginalization
+
+            # compute codeword estimates, using b
+            c = b + ch
+            c[cw >= 0] = 0
+            c[cw < 0] = 1
+
+            # if word is valid, return message estimate and exit
+            if LDPC.is_codeword(H, c):
+                return c[0:k]
+
+            ### FORWARD -> variable nodes update
+
+            for (i, j), _ in H.items():
+                F[i, j] = b[i] - B[i, j] + ch[j]
+
+            ### BACKWARD -> check nodes update
+
+            # compute total row sign
+            row_signs = F.apply_on_rows(LDPC.global_sign)
+
+            # transform all values in F, which is valid
+            # since they will be rewritten in next cycle
+            F.update_all(LDPC.phi_tilde_vector)
+
+            # precompute total phi value on F rows
+            row_phi = F.apply_on_rows(sum)
+
+            # compute sum of phi values in the whole row
+            for (i, j), _ in H.items():
+                # check if global LLR sign is positive or negative
+                if F[i, j] < 0:
+                    current_row_sign = - row_signs[i]
+                else:
+                    current_row_sign = row_signs[i]
+
+                # note that F[i, j] contains precoputed phi functions
+                B[i, j] = current_row_sign * \
+                          LDPC.phi_tilde(row_phi[i] - F[i, j])
+
+        # valid codeword was not found up to max_iterations so declare
+        # failure, returning a vector that, by NaN specification, fails
+        # all equality tests with common numbers
+        u = np.empty( (k,) )
+        u.fill(np.nan)
+        return u
+
+    return decode
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def phi_tilde(np.ndarray[np.double_t, ndim=1] x):
+cpdef phi_tilde_vector(np.ndarray[np.double_t, ndim=1] x):
     '''
     Compute function phi tilde
 
