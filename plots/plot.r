1d7efb9a9a6fb8bf24412ef15897596f8080b060
Enrico Lovisotto <enricolovisotto@gmail.com>
2017-12-22 20:24:18 +0100

    Switched to log y scale in plot, with new results

--- plots/plot.r
+++ plots/plot.r
@@ -8,24 +8,27 @@ source('plots/utils.r')
 
 data <- read.csv('results/SNRvsPe.csv')
 
-## select only a code length currently
-data <- data[data$n == 576, ]
-
 data$Pe <- data$Perror + data$Pfailure
 
-p <- ggplot(aes(x = SNR, y = Pe), data=data) +
-  geom_point() +
+p <- ggplot(data = data[data$Pe != 0,],
+           mapping = aes(x = SNR,
+                         y = Pe,
+                         color = n,
+                         group = n)) +
+  geom_line() +
+  scale_y_log10() + ## this needs data$Pe != 0
   scale_x_continuous(breaks = unique(data$SNR)) +
 
   ## highlight "always correct" and "always bad" points
-  geom_point(data = data[data$Pe == 0,], colour = "green") +
-  geom_point(data = data[data$Pe == 1,], colour = "red") +
+  ## geom_point(data = data[data$Pe == 0,], colour = "green") +
+  ## geom_point(data = data[data$Pe == 1,], colour = "red") +
+
   facet_wrap(~ rate, ncol = 2) +
   my_theme()
 
-ggsave(plot=p,
-       filename='plots/figures/SNRvsPe.png',
-       unit='cm',
-       device='png')
+## ggsave(plot=p,
+##        filename='plots/figures/SNRvsPe.png',
+##        unit='cm',
+##        device='png')
 
 p
