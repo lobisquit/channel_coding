1c90eae5204302494e5d84f023a903ac5f849e6d
Enrico Lovisotto <enricolovisotto@gmail.com>
2017-12-26 23:31:18 +0100

    Adapted plot scripts to new format
    Needed to change slightly output format, to write cleaner code

--- plots/plot.r
+++ plots/plot.r
@@ -6,22 +6,35 @@ library(gridExtra)
 
 source('plots/utils.r')
 
-data <- read.csv('results/SNRvsPe.csv')
+data <- read.csv(
+  'results/SNRvsPe.csv',
+  stringsAsFactors=FALSE)
 
-data$Pe <- data$Perror + data$Pfailure
+## convert logical strings to 1 and 0
+data$is_error[data$is_error == "True"] = 1
+data$is_error[data$is_error == "False"] = 0
+data$is_error = sapply(data$is_error, as.numeric)
 
-p <- ggplot(data = data[data$Pe != 0,],
+## convert n to cathegoric variable,
+## not to display continuous colorbar
+data$n <- sapply(data$n, as.character)
+
+## count errors and number of message passing iteration
+## per (n, rate, SNR) configuration, keeping number
+## of words and "time per word" information as well
+error_detection <- aggregate(
+  cbind(iterations, is_error) ~ n + rate + SNR + n.words + time.per.word, data, mean)
+
+p <- ggplot(data = error_detection[error_detection$is_error != 0,],
            mapping = aes(x = SNR,
-                         y = Pe,
+                         y = is_error,
                          color = n,
                          group = n)) +
   geom_line() +
-  scale_y_log10() + ## this needs data$Pe != 0
-  scale_x_continuous(breaks = unique(data$SNR)) +
-
-  ## highlight "always correct" and "always bad" points
-  ## geom_point(data = data[data$Pe == 0,], colour = "green") +
-  ## geom_point(data = data[data$Pe == 1,], colour = "red") +
+  scale_y_log10(breaks = 10^seq(0, -5),
+                labels = trans_format('log10', math_format(10^.x))) +
+  scale_x_continuous(breaks = unique(data$SNR),
+                     labels = function(x) round(x, digits=2)) +
 
   facet_wrap(~ rate, ncol = 2) +
   my_theme() +
