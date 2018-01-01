4edcd6820925fc8071d1d5784d6c2c4ad9fdbf8a
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-01-01 23:49:34 +0100

    Added plots for basics of Decoder

---
+++ plot-phi.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rcParams
+
+from LDPC import phi_tilde_vector
+
+x = np.logspace(-6, 2, num=100)
+y = phi_tilde_vector(x)
+
+plt.style.use('dark_background')
+plt.figure(0, figsize=(5, 3.1))
+plt.semilogx(x, y, color='#ff6e64')
+plt.grid(linestyle='--', linewidth=0.5)
+plt.ylabel('y', rotation=0)
+plt.xlabel('x')
+plt.tight_layout()
+plt.savefig('plots/figures/phi_tilde.pdf', transparent=True)
+plt.show()
