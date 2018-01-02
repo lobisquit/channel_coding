cac1e834497819ed3f47cefc2f5c9cba32c73072
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-01-02 15:38:13 +0100

    Fixed plots for dark background

--- plots/plot.r
+++ plots/plot.r
@@ -3,6 +3,7 @@ library(ggplot2)
 library(scales)
 library(extrafont)
 library(gridExtra)
+library(latex2exp)
 
 source('plots/utils.r')
 
@@ -16,23 +17,47 @@ error_detection <- aggregate(
 
 p <- ggplot(data = error_detection[error_detection$errors != 0,],
            mapping = aes(x = SNR,
-                         y = time.per.word,
+                         y = errors,
                          color = n,
                          group = n)) +
+  ## axis and legend labels
+  xlab(TeX('$E_b / N_o$ \\[dB\\]')) +
+  ylab('Packet Error Rate') +
+  labs(colour = 'Code lengths') +
+
   geom_line() +
-  scale_y_log10(breaks = 10^seq(0, -5),
+  facet_wrap(~ rate, nrow = 2) +
+  scale_y_log10(breaks = 10^seq(10, -10),
                 labels = trans_format('log10', math_format(10^.x))) +
   scale_x_continuous(breaks = unique(data$SNR),
                      labels = function(x) round(x, digits=2)) +
-
-  facet_wrap(~ rate, ncol = 2) +
-  my_theme() +
   theme(
-    plot.background = element_rect(fill = 'transparent')
-  )
+    rect = element_rect(fill = 'black'),
+    panel.background = element_rect(fill = 'transparent', colour = NA),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(colour='grey10'),
+
+    axis.text = element_text(colour = 'grey50'),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+
+    legend.background = element_rect(fill = 'transparent', colour = NA),
+    legend.title = element_text(colour = 'grey60',
+                                size = rel(1.1),
+                                vjust=1),
+    legend.text = element_text(colour  ='grey60'),
+
+    strip.background = element_rect(fill = 'grey10', colour = NA),
+    strip.text = element_text(colour = 'grey50'),
+
+    axis.title = element_text(colour = 'grey60', size = rel(1.1)),
+    )
+
+
 
 ## ggsave(plot = p,
 ##        filename = 'plots/figures/SNRvsPe.png',
+##        width = 10,
+##        height = 8,
 ##        unit = 'cm',
 ##        device = 'png',
 ##        bg = 'transparent')
--- plots/utils.r
+++ plots/utils.r
@@ -2,28 +2,4 @@ library(reshape2)
 library(ggplot2)
 library(scales)
 library(extrafont)
-
-my_theme = function() {
-  theme(
-    ## set everything to serif, we are good people after all
-    text = element_text("Charter"),
-
-    ## axis title size
-    axis.title.x = element_text(size = 12, margin = margin(t = 15)),
-    axis.title.y = element_text(size = 12, margin = margin(r = 15)),
-
-    ## set subtitles preferences, when facet griding
-    strip.background = element_rect(fill="white"),
-    strip.text = element_text(size = 10),
-
-    ## plot area setting
-    panel.background = element_rect(fill = "#E6E6E6"),
-    panel.grid.major = element_line(colour = "#666666", size = 0.1),
-    panel.grid.minor = element_blank(),
-
-    ## legend settings: lines background, title
-    legend.key = element_blank(),
-    legend.title = element_blank(),
-    legend.text = element_text(size = 10),
-    )
-  }
+library(gridExtra)
